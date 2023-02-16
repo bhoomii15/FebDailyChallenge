15deb5d68a9502664077382266141ca01510c91c
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-02-16 20:18:36 +0530

    day47

---
+++ Feb16.java
@@ -0,0 +1,15 @@
+package com.feb;
+
+import javax.swing.tree.TreeNode;
+
+public class Feb16 {
+    public static void main(String[] args) {
+
+    }
+//    static int maxDepth(TreeNode root) {
+//        if (root == null)
+//            return 0;
+//        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+//    }
+
+}
