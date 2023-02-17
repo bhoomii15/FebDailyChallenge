8877a8ba5a7d538d86b1b695633c8fe9dac97c23
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-02-17 13:40:01 +0530

    day48

---
+++ Feb17.java
@@ -0,0 +1,28 @@
+package com.feb;
+
+public class Feb17 {
+    public static void main(String[] args) {
+
+    }
+
+
+//        public int minDiffInBST(TreeNode root) {
+//            inorder(root);
+//            return ans;
+//        }
+//
+//        private int ans = Integer.MAX_VALUE;
+//        private Integer pred = null;
+//
+//        private void inorder(TreeNode root) {
+//            if (root == null)
+//                return;
+//
+//            inorder(root.left);
+//            if (pred != null)
+//                ans = Math.min(ans, root.val - pred);
+//            pred = root.val;
+//            inorder(root.right);
+//        }
+    }
+
