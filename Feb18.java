d37b182400fb27df727e08e78193fb1ee2058763
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-02-20 21:19:12 +0530

    day49-51

---
+++ Feb18.java
@@ -0,0 +1,18 @@
+package com.feb;
+
+public class Feb18 {
+    public static void main(String[] args) {
+
+    }
+
+//    public TreeNode invertTree(TreeNode root) {
+//        if (root == null)
+//            return null;
+//
+//        TreeNode left = root.left;
+//        TreeNode right = root.right;
+//        root.left = invertTree(right);
+//        root.right = invertTree(left);
+//        return root;
+//    }
+}
